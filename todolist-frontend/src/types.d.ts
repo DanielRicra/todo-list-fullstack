6ee1acf4380a098a0decad47a2feed26138624c6
DanielRicra <daniel.int@outlook.com>
2023-09-30 21:05:32 -0500

    Add types file

---
+++ todolist-frontend/src/types.d.ts
@@ -0,0 +1,24 @@
+export type AuthUser = {
+   email: string;
+   userId: number;
+   name: string;
+};
+
+export interface TaskList {
+  taskListId: number
+  name: string
+  userId: number
+  tasks: Task[]
+}
+
+export interface Task {
+  taskId: number
+  name: string
+  state: boolean
+  createdAt: string
+  dueDate?: string
+  note?: string
+  completedAt?: string
+  taskListId: number
+  important?: boolean
+}
