b6db41a93011f6c4e85c45ea482315180aed080b
DanielRicra <daniel.int@outlook.com>
2023-10-01 22:21:55 -0500

    Update App.tsx to add react quey provider
    - Add `QueryClientProvider` to the app.

--- todolist-frontend/src/App.tsx
+++ todolist-frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import { RouterProvider } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
-import './App.scss';
-import { UserContextProvider } from './providers/User';
-import { router } from './routes/router';
+import "./App.scss";
+import { UserContextProvider } from "./providers/User";
+
+const queryClient = new QueryClient();
 
 const App = () => {
    return (
-      <UserContextProvider>
-         <RouterProvider router={router} fallbackElement={<p>Initial...</p>} />
-      </UserContextProvider>
+      <QueryClientProvider client={queryClient}>
+         <UserContextProvider>
+            <RouterProvider
+               router={router}
+               fallbackElement={<p>Initial...</p>}
+            />
+         </UserContextProvider>
+      </QueryClientProvider>
    );
 };
 
