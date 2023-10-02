cf78b15285e67c040ca2a58a7c3942210c486e22
DanielRicra <daniel.int@outlook.com>
2023-10-01 22:29:08 -0500

    Fix the redirect route, login for signin

--- todolist-frontend/src/routes/guards/ProtectedRoutes.tsx
+++ todolist-frontend/src/routes/guards/ProtectedRoutes.tsx
@@ -1,11 +1,11 @@
-import { Outlet, Navigate } from 'react-router-dom';
-import { useUserContext } from '../../providers/User';
+import { Outlet, Navigate } from "react-router-dom";
+import { useUserContext } from "../../providers/User";
 
 const ProtectedRoutes = () => {
    const { user } = useUserContext();
 
    if (!user) {
-      return <Navigate to="/login" />
+      return <Navigate to="/signin" />;
    }
 
    return (
