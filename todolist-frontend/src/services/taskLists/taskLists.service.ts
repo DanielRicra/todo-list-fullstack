39c56119704fc4950a1f0d85e392399f526a855f
DanielRicra <daniel.int@outlook.com>
2023-10-07 16:32:21 -0500

    Update taskList.service
    - Rename `getByUserId` to  `getManyByUserId`.
    - Set default type to `create` function.

--- todolist-frontend/src/services/taskLists/taskLists.service.ts
+++ todolist-frontend/src/services/taskLists/taskLists.service.ts
@@ -2,11 +2,11 @@ import { TaskList } from '../../types';
 import { ApiRequest } from '../api';
 
 export const TaskListService = {
-   getByUserId: async (userId: string) =>
+   getManyByUserId: async (userId: number) =>
       ApiRequest.get(`/taskList/user?id=${userId}`),
    getOne: async (taskListId: string) =>
       ApiRequest.get(`/taskList/${taskListId}`),
-   create: async <T>(body: T) => ApiRequest.post<T>('/taskList', body),
+   create: async (body: Partial<TaskList>) => ApiRequest.post<Partial<TaskList>>('/taskList', body),
    update: async (body: Partial<TaskList>, taskListId: string) =>
       ApiRequest.put<Partial<TaskList>>(`/taskList/${taskListId}`, body),
    delete: async (taskListId: string) =>
