4fcc9674e87c8e342336b1900f9e4d6aa6a63d35
DanielRicra <daniel.int@outlook.com>
2023-10-07 16:27:25 -0500

    Add welcome page for '/dashboard'
    This welcome page is in charge of fetch the user taskList.
    If there is one task list then redirect to `/dashboard/{taskListId}`
    If there is no taskLists show this welcome page.

--- todolist-frontend/src/pages/Dashboard/components/Welcome.tsx
+++ todolist-frontend/src/pages/Dashboard/components/Welcome.tsx
@@ -1,4 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+import { useGetUserTaskLists } from "../../../hooks/useTaskLists";
+import { useUserContext } from "../../../providers/User";
+import { useEffect } from "react";
+
 const Welcome = () => {
-   return <div>Welcome</div>;
+   const navigate = useNavigate();
+   const { user } = useUserContext();
+
+   const { data: taskLists, isLoading } = useGetUserTaskLists(
+      user?.userId ?? NaN
+   );
+
+   useEffect(() => {
+      if (taskLists?.[0].taskListId && !isLoading) {
+         navigate(`/dashboard/${taskLists[0].taskListId}`)
+      }
+   }, [taskLists, isLoading]);
+
+   return (
+      <div className="welcome">
+         <h2>Welcome to the Todo List app</h2>
+         <p>Please create a new TaskList, on the left side bar, to add new tasks</p>
+      </div>
+   );
 };
 export default Welcome;
