caa41bdca879d4a6d489c626167c1b927dd4e7d3
DanielRicra <daniel.int@outlook.com>
2023-10-07 16:20:20 -0500

    Update Sidebar to make request with react-query
    - To fetch all TaskLists, `useGetUserTaskLists` was used.
    -To create a new taskList `useCreateTaskList` was used.

--- todolist-frontend/src/components/Sidebar/Sidebar.tsx
+++ todolist-frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,24 +1,29 @@
-import { useState } from 'react';
-import AddSharpIcon from '@mui/icons-material/AddSharp';
-import GradeOutlinedIcon from '@mui/icons-material/GradeOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import ListRoundedIcon from '@mui/icons-material/ListRounded';
+import { useState } from "react";
+import AddSharpIcon from "@mui/icons-material/AddSharp";
+import GradeOutlinedIcon from "@mui/icons-material/GradeOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import ListRoundedIcon from "@mui/icons-material/ListRounded";
 import {
+   Alert,
    Button,
    Dialog,
    DialogActions,
    DialogContent,
    DialogTitle,
+   Skeleton,
    TextField,
-} from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+} from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
-import './Sidebar.scss';
-import { IMPORTANT, PLANNED } from '../../constants/taskListId';
-import { TaskListService } from '../../services';
-import type { TaskList } from '../../types';
-import { Link, useParams } from 'react-router-dom';
+import "./Sidebar.scss";
+import { IMPORTANT, PLANNED } from "../../constants/taskListId";
+import { Link, useParams } from "react-router-dom";
+import {
+   useCreateTaskList,
+   useGetUserTaskLists,
+} from "../../hooks/useTaskLists";
+import { useUserContext } from "../../providers/User";
 
 interface SidebarProps {
    showSidebar: boolean;
@@ -27,40 +32,41 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ showSidebar, setShowSidebar }) => {
    const [openFormList, setOpenFormList] = useState(false);
-   const [taskListName, setTaskListName] = useState('');
-   const taskLists: TaskList[] = [];
+   const [taskListName, setTaskListName] = useState("");
+
+   const { user } = useUserContext();
+   const { data: taskLists, status } = useGetUserTaskLists(user?.userId ?? 0);
+   const { mutate, isLoading: isCreatingTaskList } = useCreateTaskList();
+
    const { taskListId } = useParams();
-   const user = { userId: '1' };
 
    const dark = createTheme({
       palette: {
-         mode: 'dark',
+         mode: "dark",
          primary: {
-            main: '#454f94',
+            main: "#454f94",
          },
          secondary: {
-            main: '#E65F2B',
+            main: "#E65F2B",
          },
-         error: { main: '#e23a23' },
+         error: { main: "#e23a23" },
          background: {
-            default: '#1b1f3a',
-            paper: '#292f57',
+            default: "#1b1f3a",
+            paper: "#292f57",
          },
       },
    });
 
    const createNewTaskList = () => {
-      TaskListService.create({ name: taskListName, userId: user?.userId })
-         .then((_response) => {
-            console.log('Implement this create new task list');
-         })
-         .catch((error) => {
-            console.log(error);
-         })
-         .finally(() => {
-            setTaskListName('');
-            setOpenFormList(false);
-         });
+      mutate(
+         { name: taskListName, userId: user?.userId },
+         {
+            onSuccess: () => {
+               setTaskListName("");
+               setOpenFormList(false);
+            },
+         }
+      );
    };
 
    const handleFormListClose = () => {
@@ -69,65 +75,66 @@ const Sidebar: React.FC<SidebarProps> = ({ showSidebar, setShowSidebar }) => {
 
    return (
       <>
-         <div className={'sidebar' + (showSidebar ? ' show' : '')}>
+         <div className={"sidebar" + (showSidebar ? " show" : "")}>
             <div
-               className='sidebar__menu-icon'
+               className="sidebar__menu-icon"
                onClick={() => setShowSidebar(false)}
             >
                <MenuOutlinedIcon />
             </div>
 
-            <h1 className='h1 sidebar-container'>Ts To Do</h1>
-            <div className='sidebar__menu'>
+            <h1 className="h1 sidebar-container">Ts To Do</h1>
+            <div className="sidebar__menu">
                <ul>
                   <li
                      className={
                         taskListId === IMPORTANT.toString()
-                           ? 'sidebar-container active'
-                           : 'sidebar-container'
+                           ? "sidebar-container active"
+                           : "sidebar-container"
                      }
                   >
-                     <Link to='/dashboard/tasks/important'>
-                        <GradeOutlinedIcon fontSize='small' />
+                     <Link to="/dashboard/tasks/important">
+                        <GradeOutlinedIcon fontSize="small" />
                         <p>Important</p>
                      </Link>
                   </li>
                   <li
                      className={
                         taskListId === PLANNED.toString()
-                           ? 'sidebar-container active'
-                           : 'sidebar-container'
+                           ? "sidebar-container active"
+                           : "sidebar-container"
                      }
                   >
-                     <Link to='/dashboard/tasks/planned'>
-                        <CalendarMonthOutlinedIcon fontSize='small' />
+                     <Link to="/dashboard/tasks/planned">
+                        <CalendarMonthOutlinedIcon fontSize="small" />
                         <p>Planned</p>
                      </Link>
                   </li>
                </ul>
             </div>
 
-            <div className='sidebar__tasklists'>
-               {taskLists?.length === 0 && (
-                  <div className='sidebar__tasklists-empty'>
+            <div className="sidebar__tasklists">
+               {status === "loading" ? <Skeleton /> : null}
+               {status === "success" && taskLists?.length === 0 ? (
+                  <div className="sidebar__tasklists-empty">
                      There is no list to save tasks yet, create one to start
                      saving tasks. Click in 'New List' at the bottom left
                   </div>
-               )}
-               <ul>
-                  {taskLists &&
-                     taskLists.map((taskList) => (
+               ) : null}
+               {status === "success" && taskLists?.length > 0 ? (
+                  <ul>
+                     {taskLists.map((taskList) => (
                         <li
                            key={taskList.taskListId}
                            className={
-                              'sidebar-container ' +
+                              "sidebar-container " +
                               (taskList.taskListId.toString() === taskListId
-                                 ? 'active'
-                                 : '')
+                                 ? "active"
+                                 : "")
                            }
                         >
                            <Link to={`/dashboard/${taskList.taskListId}`}>
-                              <ListRoundedIcon fontSize='small' />
+                              <ListRoundedIcon fontSize="small" />
                               <p>{taskList.name}</p>
                               {taskList?.tasks?.length > 0 && (
                                  <span>
@@ -141,12 +148,19 @@ const Sidebar: React.FC<SidebarProps> = ({ showSidebar, setShowSidebar }) => {
                            </Link>
                         </li>
                      ))}
-               </ul>
+                  </ul>
+               ) : (
+                  <div>
+                     <Alert variant="outlined" severity="error">
+                        Something went wrong.
+                     </Alert>
+                  </div>
+               )}
             </div>
 
-            <div className='sidebar__create-tasklist'>
-               <button type='button' onClick={() => setOpenFormList(true)}>
-                  <AddSharpIcon fontSize='small' /> New List
+            <div className="sidebar__create-tasklist">
+               <button type="button" onClick={() => setOpenFormList(true)}>
+                  <AddSharpIcon fontSize="small" /> New List
                </button>
             </div>
 
@@ -155,19 +169,19 @@ const Sidebar: React.FC<SidebarProps> = ({ showSidebar, setShowSidebar }) => {
                   open={openFormList}
                   onClose={handleFormListClose}
                   fullWidth
-                  color='primary'
+                  color="primary"
                >
                   <DialogTitle>Create new Task List</DialogTitle>
                   <DialogContent>
                      <TextField
                         autoFocus
-                        margin='dense'
-                        id='name'
-                        label='Task List Name'
-                        type='text'
+                        margin="dense"
+                        id="name"
+                        label="Task List Name"
+                        type="text"
                         fullWidth
-                        color='secondary'
-                        variant='standard'
+                        color="secondary"
+                        variant="standard"
                         value={taskListName}
                         onChange={(e) => setTaskListName(e.target.value)}
                      />
@@ -175,17 +189,19 @@ const Sidebar: React.FC<SidebarProps> = ({ showSidebar, setShowSidebar }) => {
                   <DialogActions>
                      <Button
                         onClick={handleFormListClose}
-                        color='secondary'
-                        variant='contained'
+                        color="secondary"
+                        variant="contained"
+                        disabled={isCreatingTaskList}
                      >
                         Cancel
                      </Button>
                      <Button
                         onClick={createNewTaskList}
-                        color='primary'
-                        variant='contained'
+                        color="primary"
+                        variant="contained"
+                        disabled={isCreatingTaskList}
                      >
-                        Create
+                        {isCreatingTaskList ? "Creating..." : "Create"}
                      </Button>
                   </DialogActions>
                </Dialog>
@@ -194,7 +210,7 @@ const Sidebar: React.FC<SidebarProps> = ({ showSidebar, setShowSidebar }) => {
 
          {showSidebar && (
             <div
-               className='full-overlay'
+               className="full-overlay"
                onClick={() => setShowSidebar(false)}
             />
          )}
