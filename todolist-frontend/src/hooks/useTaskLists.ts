10531cbccfc9304bdab3017c9655b2966302d803
DanielRicra <daniel.int@outlook.com>
2023-10-08 22:58:21 -0500

    Update useTaskLists file
    Add hook `useGetTaskList`

--- todolist-frontend/src/hooks/useTaskLists.ts
+++ todolist-frontend/src/hooks/useTaskLists.ts
@@ -2,26 +2,30 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { TaskListService } from "../services/taskLists/taskLists.service";
 import type { TaskList } from "../types";
+import { TaskService } from "../services";
 
 const taskListsKey = "taskLists";
 const taskListKey = "taskList";
 
-export function useGetUserTaskLists(userId: number) {
+export function useGetUserTaskLists(userId: number, enabled = true) {
    return useQuery({
       queryKey: [taskListsKey, userId],
       queryFn: async () => {
          const response = await TaskListService.getManyByUserId(userId);
          return response.data as TaskList[];
       },
+      enabled,
    });
 }
 
 export function useCreateTaskList() {
    const queryClient = useQueryClient();
-   return useMutation({ 
+   return useMutation({
       mutationFn: TaskListService.create,
-      onSuccess: () => { queryClient.invalidateQueries({ queryKey: [taskListsKey]})}
-   })
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: [taskListsKey] });
+      },
+   });
 }
 
 export function useGetTaskList(taskListId: string) {
@@ -30,6 +34,18 @@ export function useGetTaskList(taskListId: string) {
       queryFn: async () => {
          const response = await TaskListService.getOne(taskListId);
          return response.data as TaskList;
+      },
+   });
+}
+
+export function useCreateTask() {
+   const queryClient = useQueryClient();
+   return useMutation({
+      mutationFn: TaskService.create,
+      onSuccess: (response) => {
+         queryClient.invalidateQueries({
+            queryKey: [taskListKey, `${response.data.taskListId}`],
+         });
       },
    });
 }
