8091b0ac7804e2ba81d886c99ff5661750a6f56f
DanielRicra <danielricra.dev@gmail.com>
2023-10-21 17:07:17 -0500

    Remove the unused async and await in api.ts

--- todolist-frontend/src/services/api.ts
+++ todolist-frontend/src/services/api.ts
@@ -1,15 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
    baseURL: BASE_URL,
-   headers: { 'Content-Type': 'application/json' },
+   headers: {
+      "Content-Type": "application/json",
+   },
 });
 
 export const ApiRequest = {
-   get: async (url: string) => await api.get(url),
-   post: async <T>(url: string, body: T) => await api.post(url, body),
-   put: async <T>(url: string, body: T) => await api.put(url, body),
-   delete: async (url: string) => await api.delete(url),
+   get: (url: string) => api.get(url),
+   post: <T>(url: string, body: T) => api.post(url, body),
+   put: <T>(url: string, body: T) => api.put(url, body),
+   delete: (url: string) => api.delete(url),
 };
