fabdf5925c5ebe971d1e16f3d685e9e426b8701e
DanielRicra <danielricra.dev@gmail.com>
2023-10-21 23:06:19 -0500

    Update use-task-lists.ts
    - Add `useUpdateTaskList` and use toast.
    - Add `useDeleteTaskList` and use toast.

--- todolist-frontend/src/hooks/use-task-lists.ts
+++ todolist-frontend/src/hooks/use-task-lists.ts
@@ -3,6 +3,8 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { TaskListService } from "../services/taskLists/task-lists.service";
 import type { TaskList } from "../types";
 import { taskListKey, taskListsKey } from "../constants/query-keys";
+import { toast } from "sonner";
+import { AxiosError } from "axios";
 
 export function useGetUserTaskLists(userId: number, enabled = true) {
    return useQuery({
@@ -31,6 +33,47 @@ export function useGetTaskList(taskListId: string) {
       queryFn: async () => {
          const response = await TaskListService.getOne(taskListId);
          return response.data as TaskList;
+      },
+   });
+}
+
+export function useUpdateTaskList(userId?: number | null) {
+   const queryClient = useQueryClient();
+   return useMutation({
+      mutationFn: TaskListService.update,
+      onSuccess: () => {
+         toast.success("Task list updated successfully");
+         return Promise.all([
+            queryClient.invalidateQueries({ queryKey: [taskListKey] }),
+            queryClient.invalidateQueries({ queryKey: [taskListsKey, userId] }),
+         ]);
+      },
+      onError: (error) => {
+         const defaultMessage = "Something went wrong, try again later.";
+         toast.error(
+            error instanceof AxiosError
+               ? error.response?.data.message || defaultMessage
+               : defaultMessage
+         );
+      },
+   });
+}
+
+export function useDeleteTaskList(userId?: number | null) {
+   const queryClient = useQueryClient();
+   return useMutation({
+      mutationFn: TaskListService.delete,
+      onSuccess: () => {
+         queryClient.invalidateQueries({ queryKey: [taskListsKey, userId] });
+         toast.success("Task list deleted successfully");
+      },
+      onError: (error) => {
+         const defaultMessage = "Something went wrong, try again later.";
+         toast.error(
+            error instanceof AxiosError
+               ? error.response?.data.message || defaultMessage
+               : defaultMessage
+         );
       },
    });
 }
